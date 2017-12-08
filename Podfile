37c37a0702b0ad64099524606fc4067a8891c800
Angus Moore <angusmoore@macbook-pro.local>
2017-12-08 00:06:04 +0000

    9/8 added, all features successfully implemented, near if not final version

---
+++ Podfile
@@ -0,0 +1,3 @@
+target ‘ELEC2660 Project’ do
+  pod 'EZAudio', '~> 1.1'
+end
---
+++ Pods/Target Support Files/Pods-ELEC2660 Project/Pods-ELEC2660 Project-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ELEC2660_Project : NSObject
+@end
+@implementation PodsDummy_Pods_ELEC2660_Project
+@end
